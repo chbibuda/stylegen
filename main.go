22437143e44487175aed9b454a865169e7db5698
Simon Chiu <simon.garhing@gmail.com>
2015-09-26 11:37:14 +1000

    Added CSS file, restructured main.go to separate public resource routes from main page route

--- main.go
+++ main.go
@@ -3,16 +3,48 @@ package main
 import (
 	"html/template"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
+var styles []string = []string{
+	"Popping",
+	"Boogaloo",
+	"Scarecrow",
+	"Tutting",
+	"Waving",
+	"Glides",
+	"Puppet",
+	"Crazy Legs",
+	"Snaking",
+	"Strobing",
+	"Ticking",
+	"Dime Stop",
+	"Saccing",
+	"Air Posing",
+	"Robot",
+	"Digits",
+	"Sleepy Style",
+	"Toyman",
+	"Banging",
+	"Strutting",
+}
+
 func main() {
-	http.HandleFunc("/", someFunc)
+	rand.Seed(time.Now().UTC().UnixNano())
+	http.HandleFunc("/", homePage)
+	// css, js, img files
+	http.HandleFunc("/public/", publicResources)
 	http.ListenAndServe(":8080", nil)
 }
 
-func someFunc(w http.ResponseWriter, req *http.Request) {
+func homePage(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path[1:]
+	log.Printf("HTTP request for %v\n", string(path))
+
 	first, second := generateStyles()
 
 	data := map[string]string{
@@ -22,36 +54,45 @@ func someFunc(w http.ResponseWriter, req *http.Request) {
 
 	page, err := ioutil.ReadFile("templates/home.template")
 
-	w.Header().Add("Content Type", "text/html")
+	w.Header().Add("Content-Type", "text/html")
 	tmpl, err := template.New("anyNameForTemplate").Parse(string(page))
+
 	if err == nil {
 		tmpl.Execute(w, data)
 	}
 }
 
-func generateStyles() (string, string) {
-	styles := []string{
-		"Popping",
-		"Boogaloo",
-		"Scarecrow",
-		"Tutting",
-		"Waving",
-		"Glides",
-		"Puppet",
-		"Crazy Legs",
-		"Snaking",
-		"Strobing",
-		"Ticking",
-		"Dime Stop",
-		"Saccing",
-		"Air Posing",
-		"Robot",
-		"Digits",
-		"Sleepy Style",
-		"Toyman",
-		"Banging",
-	}
+func publicResources(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path[1:]
+	log.Printf("HTTP request for %v\n", string(path))
 
+	page, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		var contentType string
+
+		if strings.HasSuffix(string(path), ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".svg") {
+			contentType = "image/svg+xml"
+		} else {
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content-Type", contentType)
+		w.Write(page)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("Error 404 - " + http.StatusText(404)))
+	}
+}
+
+func generateStyles() (string, string) {
 	first := rand.Intn(len(styles))
 	second := rand.Intn(len(styles))
 
